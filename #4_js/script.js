44871d868051c36c1d596f2d32bf8b881938c91d
alex nizov <nizovoy.shura@mail.ru>
2022-01-25 14:28:19 +0300

    ready

--- #4_js/script.js
+++ #4_js/script.js
@@ -1,23 +1,25 @@
 class Obj {
-    simvol (number, simvOl) {
-        if (simvOl.length == 1) {
+    _getRandomLetter() {
+        let randomSimvolAdd = "±§!@#$%^&*+~?/><;:'\"";
+        return randomSimvolAdd[Math.floor(Math.random() * randomSimvolAdd.length)];
+      }
+    simvol (number, simvOl, question) {
+        if(question == true){
+          simvOl = this._getRandomLetter()
             for (let index = 0; index < number; index++) {
                 console.log(Array(index).fill(simvOl).join(''))
             } 
         } else {
-            console.log('Ошибка ввода символа')
+            if (simvOl.length == 1) {
+                for (let index = 0; index < number; index++) {
+                   console.log(Array(index).fill(simvOl).join(''))
+                } 
+            } else {
+               console.log('Ошибка ввода символа')
+            }
         }
     }
 }
-const user = new Obj();
-user.simvol(6,'#');
-user.simvol(6,'#21');
 
-let randomSimvolAdd = "±§!@#$%^&*+~?/><;:'\"";
-function getRandomLetter() {
-  return randomSimvolAdd[Math.floor(Math.random() * randomSimvolAdd.length)];
-}
-user.simvol(8, getRandomLetter())
-user.simvol(4, getRandomLetter())
-user.simvol(5, getRandomLetter())
-user.simvol(6, getRandomLetter())
+const user = new Obj();
+user.simvol(6, 'Z', confirm('Использовать рондомный символ?'))
